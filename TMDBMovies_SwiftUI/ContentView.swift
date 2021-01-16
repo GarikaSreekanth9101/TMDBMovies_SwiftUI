b1ea984b90a1ec3a5b5e745bc44a313ba81fe7d7
Garika Sreekanth <sreekanthgarika91@gmail.com>
2021-01-16 11:44:10 +0530

    Initial Commit

---
+++ TMDBMovies_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TMDBMovies_SwiftUI
+//
+//  Created by Garika Sreekanth on 16/01/21.
+//  Copyright © 2021 Garika Sreekanth. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
